edb6b2723be674cfc49f44bd4bfe57c1410ad8dc
NadirYa <nadir.yala@hotmail.com>
2023-12-08 10:09:19 -0500

    Huitième Commit

--- asm64/asm06.s
+++ asm64/asm06.s
@@ -3,8 +3,10 @@ global _start
 
 _start:
     ; Exécuter /bin/sh
-    mov eax, 11         ; syscall pour exécuter un programme
-    mov ebx, '/bin/sh'
-    xor ecx, ecx        ; pas d'arguments
-    xor edx, edx        ; pas d'environnement
-    int 0x80
+    mov eax, 59         ; syscall pour execve
+    mov rdi, bin_sh     ; adresse de la chaîne '/bin/sh'
+    xor rsi, rsi        ; pas d'arguments
+    xor rdx, rdx        ; pas d'environnement
+    syscall
+
+bin_sh db '/bin/sh', 0
